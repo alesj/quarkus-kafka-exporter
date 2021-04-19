d1615a4babec2fa58b14c623a821ae457ac0cee4
Ales Justin <ales.justin@gmail.com>
2021-04-19 13:58:59 +0200

    Add metadata refresh support.

--- README.md
+++ README.md
@@ -4,6 +4,36 @@
 
 It supports all the same metrics, apart from Zookeeper consumer groups lag.
 
+Any Kafka config should start with `kafka.`, and then use exact key from Kafka config.
+
+```
+e.g. kafka.bootstrap.servers=mykafka:9092
+```
+
+Other config includes (same as kafka_exporter)
+```
+    @ConfigProperty(name = "metrics.path", defaultValue = "/metrics")
+    String metricsPath;
+
+    @ConfigProperty(name = "prometheus.http.port", defaultValue = "9308")
+    int httpPort;
+
+    @ConfigProperty(name = "refresh.metadata", defaultValue = "PT1M")
+    Duration metadataRefreshInterval;
+
+    @ConfigProperty(name = "namespace", defaultValue = "kafka")
+    String namespace;
+
+    @ConfigProperty(name = "topic.filter", defaultValue = ".*")
+    Pattern topicFilter;
+
+    @ConfigProperty(name = "group.filter", defaultValue = ".*")
+    Pattern groupFilter;
+
+    @ConfigProperty(name = "kafka.labels")
+    Optional<String> kafkaLabels;
+```
+
 TODO ... more docs
 
 ## Build
--- server/src/main/java/io/strimzi/kafkaexporter/server/Configuration.java
+++ server/src/main/java/io/strimzi/kafkaexporter/server/Configuration.java
@@ -4,17 +4,24 @@ import io.strimzi.kafkaexporter.server.utils.AdminProvider;
 import io.strimzi.kafkaexporter.server.utils.AdminProviderImpl;
 import io.strimzi.kafkaexporter.server.utils.InjectedProperties;
 import io.strimzi.kafkaexporter.server.utils.PropertiesUtil;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import java.time.Duration;
 import java.util.Properties;
 
 /**
  * @author Ales Justin
  */
 public class Configuration {
+
+    @ConfigProperty(name = "refresh.metadata", defaultValue = "PT1M")
+    Duration metadataRefreshInterval;
+
     @Produces
     public Properties properties(InjectionPoint ip) {
         return PropertiesUtil.properties(ip);
@@ -23,7 +30,10 @@ public class Configuration {
     @Produces
     @ApplicationScoped
     public AdminProvider createAdmin(@InjectedProperties("kafka") Properties properties) {
-        return new AdminProviderImpl(properties);
+        Properties copy = new Properties();
+        copy.put(AdminClientConfig.METADATA_MAX_AGE_CONFIG, metadataRefreshInterval.toMillis());
+        copy.putAll(properties);
+        return new AdminProviderImpl(copy);
     }
 
     public void closeAdmin(@Disposes AdminProvider adminProvider) throws Exception {
