7ba0ff15a79919ecba4215f56c2c03d1dae058c3
Ales Justin <ales.justin@gmail.com>
2021-04-22 10:26:55 +0200

    Add test base.

--- server/src/test/java/io/strimzi/kafkaexporter/server/test/LoadTest.java
+++ server/src/test/java/io/strimzi/kafkaexporter/server/test/LoadTest.java
@@ -2,10 +2,7 @@ package io.strimzi.kafkaexporter.server.test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.strimzi.kafkaexporter.server.utils.AdminHandle;
-import io.strimzi.kafkaexporter.server.utils.AdminProvider;
-import io.strimzi.kafkaexporter.server.utils.InjectedProperties;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.client.WebClient;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
@@ -20,11 +17,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
 
-import javax.inject.Inject;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,7 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -40,71 +32,17 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @author Ales Justin
  */
 @QuarkusTest
-public class LoadTest {
-    private static final Logger log = LoggerFactory.getLogger(LoadTest.class);
-
-    @Inject
-    AdminProvider adminProvider;
-
-    @Inject
-    Vertx vertx;
-
-    @Inject
-    @InjectedProperties("kafka")
-    Properties properties;
-
-    private boolean isKafkaRunning() {
-        try (AdminHandle adminHandle = adminProvider.getAdminHandle()) {
-            Admin admin = adminHandle.getAdmin();
-            admin.listTopics().names().get();
-            return true;
-        } catch (Throwable ignored) {
-            return false;
-        }
-    }
+public class LoadTest extends TestBase {
 
     public static void main(String[] args) {
         LoadTest test = new LoadTest();
         test.vertx = Vertx.vertx();
         try {
-            Logging logging = (l, m, t) -> {
-                if (l == Level.INFO) {
-                    System.out.println(m);
-                } else {
-                    System.err.println(m + ":" + t);
-                }
-            };
-            test.executeMetricsRequest(10, logging);
+            test.executeMetricsRequest(10, sysLogging);
         } catch (Throwable t) {
             System.err.println("Error: " + t);
         } finally {
             test.vertx.close();
-        }
-    }
-
-    @FunctionalInterface
-    private interface Logging {
-        void log(Level level, String msg, Throwable t);
-    }
-
-    private void executeMetricsRequest(int retries, Logging logging) throws Exception {
-        CountDownLatch latch = new CountDownLatch(retries);
-        WebClient client = WebClient.create(vertx);
-        try {
-            while (retries > 0) {
-                retries--;
-                client.get(9308, "localhost", "/metrics").send(event -> {
-                    if (event.succeeded()) {
-                        logging.log(Level.INFO, "Metrics: \n" + event.result().bodyAsString(), null);
-                    } else {
-                        logging.log(Level.ERROR, "Metrics request failed ...", event.cause());
-                    }
-                    latch.countDown();
-                });
-            }
-            latch.await();
-        } finally {
-            client.close();
         }
     }
 
@@ -117,9 +55,14 @@ public class LoadTest {
     static long nTimeout = 60 * 1000; // how long we wait for msgs
     static int batch = 16;
 
+    private void assumeLoadTest() {
+        Assumptions.assumeTrue(Boolean.getBoolean("load.test"), "Enable load test: -Dload.test=true");
+        Assumptions.assumeTrue(isKafkaRunning(), "Kafka is not running");
+    }
+
     @BeforeEach
     public void init() throws Exception {
-        Assumptions.assumeTrue(isKafkaRunning());
+        assumeLoadTest();
 
         List<Consumer<String, String>> consumers = new ArrayList<>();
         List<Producer<String, String>> producers = new ArrayList<>();
@@ -195,14 +138,7 @@ public class LoadTest {
 
     @Test
     public void testLoad() throws Exception {
-        Assumptions.assumeTrue(isKafkaRunning());
-        Logging logging = (l, m, t) -> {
-            if (l == Level.INFO) {
-                log.info(m);
-            } else {
-                log.error(m, t);
-            }
-        };
-        executeMetricsRequest(5, logging);
+        assumeLoadTest();
+        executeMetricsRequest(5, slf4jLogging);
     }
 }
---
+++ server/src/test/java/io/strimzi/kafkaexporter/server/test/TestBase.java
@@ -0,0 +1,84 @@
+package io.strimzi.kafkaexporter.server.test;
+
+import io.strimzi.kafkaexporter.server.utils.AdminHandle;
+import io.strimzi.kafkaexporter.server.utils.AdminProvider;
+import io.strimzi.kafkaexporter.server.utils.InjectedProperties;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import org.apache.kafka.clients.admin.Admin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import javax.inject.Inject;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Ales Justin
+ */
+public class TestBase {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Inject
+    AdminProvider adminProvider;
+
+    @Inject
+    Vertx vertx;
+
+    @Inject
+    @InjectedProperties("kafka")
+    Properties properties;
+
+    protected boolean isKafkaRunning() {
+        try (AdminHandle adminHandle = adminProvider.getAdminHandle()) {
+            Admin admin = adminHandle.getAdmin();
+            admin.listTopics().names().get();
+            return true;
+        } catch (Throwable ignored) {
+            return false;
+        }
+    }
+
+    @FunctionalInterface
+    protected interface Logging {
+        void log(Level level, String msg, Throwable t);
+    }
+
+    protected static Logging sysLogging = (l, m, t) -> {
+        if (l == Level.INFO) {
+            System.out.println(m);
+        } else {
+            System.err.println(m + ":" + t);
+        }
+    };
+
+    protected Logging slf4jLogging = (l, m, t) -> {
+        if (l == Level.INFO) {
+            log.info(m);
+        } else {
+            log.error(m, t);
+        }
+    };
+
+    protected void executeMetricsRequest(int retries, Logging logging) throws Exception {
+        CountDownLatch latch = new CountDownLatch(retries);
+        WebClient client = WebClient.create(vertx);
+        try {
+            while (retries > 0) {
+                retries--;
+                client.get(9308, "localhost", "/metrics").send(event -> {
+                    if (event.succeeded()) {
+                        logging.log(Level.INFO, "Metrics: \n" + event.result().bodyAsString(), null);
+                    } else {
+                        logging.log(Level.ERROR, "Metrics request failed ...", event.cause());
+                    }
+                    latch.countDown();
+                });
+            }
+            latch.await();
+        } finally {
+            client.close();
+        }
+    }
+}
