ee8f60bb3b20f21cd33f584e5de4db97374f5601
Ales Justin <ales.justin@gmail.com>
2021-05-04 15:28:55 +0200

    Add Strimzi Dockerfile.

---
+++ server/src/main/docker/kafka_exporter_run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+set +x
+
+# We don't need LOG_DIR because we write no log files, but setting it to a
+# directory avoids trying to create it (and logging a permission denied error)
+if [ -n "$KAFKA_EXPORTER_HOME" ]; then
+    export LOG_DIR="$KAFKA_EXPORTER_HOME"
+fi
+
+export QUARKUS_HTTP_PORT=9404
+export QUARKUS_PROFILE=strimzi
+
+exec /deployments/run-java.sh
